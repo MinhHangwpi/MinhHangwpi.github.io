44826ea76551956912c5acb3e45fd43381088983
MinhHang <mgradetsky@wpi>
2024-04-06 20:54:31 -0400

    updated gh-pages

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,24 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import viteTsconfigPaths from 'vite-tsconfig-paths'
-// import reactPlugin from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//     // depending on your application, base can also be "/"
-//     base: '',
-//     plugins: [react(), viteTsconfigPaths()],
-//     server: {
-//         // this ensures that the browser opens upon server start
-//         open: true,
-//         // this sets a default port to 3000  
-//         port: 3000,
-//     },
-//     optimizeDeps: {
-//         include: ['react/jsx-dev-runtime', 'react/jsx-runtime'],
-//     }
-// })
-
-
 import reactPlugin from '@vitejs/plugin-react'
 
 import { defineConfig } from 'vite'
@@ -28,6 +7,7 @@ const config = async () => {
     const { default: mdx } = await import('@mdx-js/rollup')
 
     return {
+        base: "/portfolio/",
         plugins: [reactPlugin(), mdx({ remarkPlugins: [] })],
         optimizeDeps: {
             include: ['react/jsx-runtime'],
